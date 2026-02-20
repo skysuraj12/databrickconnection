13104c85433bf2dba7e35333d93f0a565ee46873
calmesuraj <itsmesuraaz@gmail.com>
2026-02-19 21:12:38 -0600

    changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,12 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8787',
-        changeOrigin: true
-      }
-    }
-  }
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+    },
+  },
 })
